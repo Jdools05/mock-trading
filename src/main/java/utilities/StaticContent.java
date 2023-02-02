1376d8005f5142786c81eb06ab877d3fa7704e56
Jordan Dooley <jmdooley05@gmail.com>
2023-02-01 20:07:24 -0600

    Made all UI pages under the path /app/

--- src/main/java/utilities/StaticContent.java
+++ src/main/java/utilities/StaticContent.java
@@ -13,41 +13,41 @@ import javax.enterprise.context.ApplicationScoped;
 public class StaticContent {
 
     @RolesAllowed({"user", "admin"})
-    @Route(path = "/home", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/home", methods = Route.HttpMethod.GET)
     void indexContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "frontend/index.html").handle(rc);
     }
 
-    @Route(path = "/login", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/login", methods = Route.HttpMethod.GET)
     void loginContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "frontend/login.html").handle(rc);
     }
 
-    @Route(path = "/whitelist", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/whitelist", methods = Route.HttpMethod.GET)
     void whitelistContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "frontend/whitelist.html").handle(rc);
     }
 
     @RolesAllowed("admin")
-    @Route(path = "/admin", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/admin", methods = Route.HttpMethod.GET)
     void adminContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "frontend/admin.html").handle(rc);
     }
 
     @PermitAll
-    @Route(path = "/reset-password", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/reset-password", methods = Route.HttpMethod.GET)
     void resetPasswordContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "frontend/reset-password.html").handle(rc);
     }
 
     @PermitAll
-    @Route(path = "/request-password-reset", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/request-password-reset", methods = Route.HttpMethod.GET)
     void requestPasswordResetContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "frontend/request-password-reset.html").handle(rc);
     }
 
     @RolesAllowed({"user", "admin"})
-    @Route(path = "/instructions", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/instructions", methods = Route.HttpMethod.GET)
     void instructionsContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "frontend/instructions.html").handle(rc);
     }
@@ -55,24 +55,24 @@ public class StaticContent {
 
 
 //    @PermitAll
-//    @Route(path = "/test", methods = Route.HttpMethod.GET)
+//    @Route(path = "/app/test", methods = Route.HttpMethod.GET)
 //    void testContent(RoutingContext rc) {
 //        StaticHandler.create(FileSystemAccess.RELATIVE, "frontend/test.html").handle(rc);
 //    }
 
     @PermitAll
-    @Route(path = "/darkmode.css", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/darkmode.css", methods = Route.HttpMethod.GET)
     void darkmodeContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "META-INF/bootstrap.min.css").handle(rc);
     }
 
     @PermitAll
-    @Route(path = "/favicon.ico", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/favicon.ico", methods = Route.HttpMethod.GET)
     void faviconContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "META-INF/favicon.ico").handle(rc);
     }
 
-    @Route(path = "/register", methods = Route.HttpMethod.GET)
+    @Route(path = "/app/register", methods = Route.HttpMethod.GET)
     void registerContent(RoutingContext rc) {
         StaticHandler.create(FileSystemAccess.RELATIVE, "frontend/register.html").handle(rc);
     }
